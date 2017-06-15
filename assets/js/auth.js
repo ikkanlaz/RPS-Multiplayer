4e7ea0b73dd473110119d9f99277c485a3402a97
ikkanlaz <andrew.lazenby@gmail.com>
2017-06-14 19:47:45 -0700

    testing google maps api

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -153,4 +153,19 @@ $("#btn-log-out").on("click", function () {
     auth.signOut();
 });
 
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://maps.googleapis.com/maps/api/geocode/json?address=94105&key=AIzaSyBxgMHK10T-YS90r9OQhsSJm_aeEFAGcZ8",
+  "method": "GET",
+  "headers": {
+    "cache-control": "no-cache",
+    "postman-token": "7d413731-c7a3-3135-2d0b-df2b1d404bf1"
+  }
+}
+
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+
 
